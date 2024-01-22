7e11bc86678ea03f8d34cc7a6b2bc68d8e378944
Azron Ahyan <azronahyan@gmail.com>
2024-01-22 23:14:45 +0000

    Calender added

---
+++ app/src/main/java/com/app/traderlogkeeper/az/MainActivity.java
@@ -0,0 +1,17 @@
+package com.app.traderlogkeeper.az;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //logger day keeper app
+
+    }
+}
